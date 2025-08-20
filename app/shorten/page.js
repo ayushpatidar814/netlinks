18f8fc6129c8be3d309f3d98cf916a4dd708d179
ayushpatidar814 <ayushpatidar814@gmail.com>
2025-08-20 12:26:42 +0530

    saved

---
+++ app/shorten/page.js
@@ -0,0 +1,57 @@
+"use client"
+import React from 'react'
+import { useState } from 'react'
+import Link from 'next/link'
+
+const shorten = () => {
+    const [url, seturl] = useState("")
+    const [shorturl, setshorturl] = useState("")
+    const [generated, setgenerated] = useState("")
+
+    const generate = () => {
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      
+      const raw = JSON.stringify({
+        "url": url,
+        "shorturl": shorturl
+      });
+      
+      const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+      };
+      
+      fetch("/api/generate", requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+            setgenerated(`${process.env.NEXT_PUBLIC_HOST}/${shorturl}`)
+            seturl("")
+            setshorturl("")
+            console.log(result)
+            alert(result.message)
+          } 
+            
+        )
+        .catch((error) => console.error(error));
+    }
+
+  return (
+    <div className='mx-auto max-w-lg bg-purple-100 my-16 p-8 rounded-lg flex flex-col gap-4'>
+        <h1 className="font-bold text-2xl">Generate your short URLs</h1>
+        <div className='flex flex-col gap-2'>
+            <input type="text" className='px-4 py-2 rounded-md focus:outline-purple-600' value={url} placeholder='Enter your URL' onChange={e=>{seturl(e.target.value)}}/>
+            <input type="text" className='px-4 py-2 rounded-md focus:outline-purple-600' value={shorturl} placeholder='Enter your preferred short URL text' onChange={e=>{setshorturl(e.target.value)}}/>
+            <button onClick={generate} className='bg-purple-500 rounded-lg shadow-lg p-3 py-1 m-3 text-white font-bold'>Generate</button>
+        </div>
+
+        {generated && <> <span className='font-bold text-lg'>Your Link</span><code>
+        <Link target="_blank" href={generated}>{generated}</Link>
+          </code></>}
+    </div>
+  )
+}
+
+export default shorten
